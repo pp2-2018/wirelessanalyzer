640cf6a06ad266011784264b2d852db1c54d54fb
Agus Alexander <agusalexander@hotmail.com>
2018-04-06 16:30:23 -0300

    fix

--- src/test/java/PcapFileReaderTest.java
+++ src/test/java/PcapFileReaderTest.java
@@ -8,20 +8,18 @@ import java.io.OutputStream;
 public class PcapFileReaderTest {
 
     private PcapFileReader reader;
+    /*
     //@Before
-    @Ignore
     public void setup(){
         String filename = "/home/lucas/Documentos/wireless-capture.pcap";
         reader = new PcapFileReader(filename);
     }
     //@Test
-    @Ignore
     public void readFile(){
         OutputStream stream = reader.read();
         System.out.println(stream.toString());
     }
-
-
+*/
 
 
 }
