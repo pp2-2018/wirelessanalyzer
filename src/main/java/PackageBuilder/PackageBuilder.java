37e3b400740d4162fbe5daf0a5e1818c5373258f
leafunes <funes.s.leandro@gmail.com>
2018-04-14 00:21:29 -0300

    buildPackage method pseudo implemented

--- src/main/java/PackageBuilder/PackageBuilder.java
+++ src/main/java/PackageBuilder/PackageBuilder.java
@@ -1,5 +1,36 @@
 package PackageBuilder;
 
+import Evaluator.Expression;
+import Model.RawPackage;
+
 public class PackageBuilder {
 
+	private PackageBuilder next;
+	private Expression<RawPackage> filter;
+	
+	
+	public PackageBuilder() {
+		
+	}
+	
+	public void setNext(PackageBuilder next){
+		if(next == null);
+			//TODO throw
+		this.next = next;
+	}
+	
+	public Package buildPackage(RawPackage rawPackage){
+		
+		if(filter.interpret(rawPackage)){
+			/*
+			 * return configurationBasedBuilder.build(rawPackage);
+			 * 
+			 */
+		}
+		if(next == null);
+			//throw
+		return next.buildPackage(rawPackage);
+		
+	}
+	
 }
