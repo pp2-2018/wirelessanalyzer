c607eecfda2c0dd4614559f4db5ffe6de23981a7
LuluNull <lucylemonyay@gmail.com>
2018-04-16 00:20:29 -0300

    Clase intervalo

---
+++ src/main/java/controller/MacAnalizer.java
@@ -0,0 +1,21 @@
+package controller;
+
+import java.time.LocalDateTime;
+
+import model.Intervalo;
+import model.MacAddress;
+
+public class MacAnalizer {
+	
+	public MacAnalizer(MacAddress mac, LocalDateTime fechaInicio, LocalDateTime fechaFin){
+		
+		Intervalo intervalo = new Intervalo(fechaInicio, fechaFin);
+		
+		
+		
+		
+	}
+	
+	
+
+}
