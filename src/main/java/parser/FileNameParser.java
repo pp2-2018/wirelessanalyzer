de3be928402b6e4a5e42bd1a4ae1dd6084090fe3
Unknown <lucas.addisi@gmail.com>
2018-04-20 19:22:09 -0300

    FileNameParse class added

--- src/main/java/parser/FileNameParser.java
+++ src/main/java/parser/FileNameParser.java
@@ -1,6 +1,7 @@
 package parser;
 
-import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
+import exceptions.MacNotMatchedException;
+import fileReader.HexStringToByteArray;
 import model.device.MacAddress;
 
 import java.util.regex.Matcher;
@@ -8,27 +9,31 @@ import java.util.regex.Pattern;
 
 public class FileNameParser { //TODO Habría que ver como generalizar
 
-    private final String macAddressregex = "";
+    private String macAddressregex = "([a-f0-9]{2}-){5}[a-f0-9]{2}"; //TODO Habría que sacar esta regex de un archivo de config
+    private HexStringToByteArray hexStringToByteArray;
     private Matcher matcher;
     private Pattern p;
 
     public FileNameParser() {
         p = Pattern.compile(macAddressregex);
+        hexStringToByteArray = new HexStringToByteArray();
     }
 
-    public MacAddress parse(String fileName){
+    public MacAddress parse(String fileName) throws MacNotMatchedException{
         matcher = p.matcher(fileName);
 
-        if (!matcher.find()) //TODO Tiene que tirar excepcion de mac address not found
-            return null;
+        if (!matcher.find())
+            throw new MacNotMatchedException();
 
-        byte[] byteMACAddress = stringTobyteArray(matcher.group(0));
+        String cadena = matcher.group(0);
+        byte[] byteMACAddress = stringTobyteArray(cadena);
 
-        return null;
+        return new MacAddress(byteMACAddress); //TODO esto está ok??
     }
 
     private byte[] stringTobyteArray(String group) {
-        return null;
+        String toConvert = group.replaceAll("-", "");
+        return hexStringToByteArray.convert(toConvert);
     }
 
 }
