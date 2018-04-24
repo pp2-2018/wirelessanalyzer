1076e7d880292e3f1af85b6561195f1a3fad92af
Unknown <lucas.addisi@gmail.com>
2018-04-24 16:37:07 -0300

    PcapFileLoader class added

---
+++ src/test/java/fileReader/PcapFileLoaderTest.java
@@ -0,0 +1,40 @@
+package fileReader;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import sun.invoke.WrapperInstance;
+import validator.FileExtensionValidator;
+
+import java.io.File;
+
+import static org.junit.Assert.*;
+
+public class PcapFileLoaderTest {
+
+    private PcapFileLoader loader;
+
+    @Before
+    public void setUp(){
+        File file = new File("test-files" + File.separator + "test-capture.pcap");
+        String extension = "pcap";
+        loader = new PcapFileLoader(file.getAbsolutePath(), extension);
+    }
+
+
+    @Test
+    public void loadingFileTest(){
+
+    }
+
+    @Ignore //FIXME No funciona
+    @Test(expected = WrongExtensionException.class)
+    public void wrongExceptionTest(){
+        File file = new File("test-files" + File.separator + "test-capture.txt");
+        String extension = "pcap";
+
+        PcapFileLoader gonnaExplode = new PcapFileLoader(file, extension);
+    }
+
+}
