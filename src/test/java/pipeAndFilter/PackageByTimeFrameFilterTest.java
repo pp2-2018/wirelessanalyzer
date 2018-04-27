d694a2b35d1e7a4d000c6f136032103f66f882c1
Agus Alexander <agusalexander@hotmail.com>
2018-04-27 16:49:45 -0300

    fix en test

--- src/test/java/pipeAndFilter/PackageByTimeFrameFilterTest.java
+++ src/test/java/pipeAndFilter/PackageByTimeFrameFilterTest.java
@@ -22,17 +22,25 @@ public class PackageByTimeFrameFilterTest {
     LocalDateTime inicio;
     LocalDateTime fin;
     Package packageA;
+    Package packageB;
     @Before
     public void setUp(){
-        inicio = LocalDateTime.of(2014, Month.JANUARY, 1, 10, 10, 30);
-        fin = LocalDateTime.of(2015, Month.JANUARY, 1, 10, 10, 30);
+        inicio = LocalDateTime.of(1999, Month.JANUARY, 20, 23, 59, 59);
+        fin = LocalDateTime.of(1999, Month.JANUARY, 26, 0, 0, 0);
+        LocalDateTime fueraDeIntervalo =
+                LocalDateTime.of(1999, Month.JANUARY, 31, 21, 3, 53);
+        LocalDateTime dentroDeIntervalo =
+                LocalDateTime.of(1999, Month.JANUARY, 25, 23, 59, 59);
 
         timeFrame = new TimeFrame(inicio, fin);
 
         byte b1[] = {  0x4a, 0x08, 0x7f, 0x41, 0x12, 0x47 };	//mac addresses
         MacAddress addra = new MacAddress(b1);
-        packageA = new Package(new TimeStamp(inicio));				//paquetes
+        packageA = new Package(new TimeStamp(dentroDeIntervalo));				//paquetes
         packageA.setMacAddress(addra);
+
+        packageB = new Package(new TimeStamp(fueraDeIntervalo));				//paquetes
+        packageB.setMacAddress(addra);
 
 
     }
@@ -54,12 +62,13 @@ public class PackageByTimeFrameFilterTest {
         PackageByTimeFrameFilter packageByTimeFrameFilter = new PackageByTimeFrameFilter
                 (pipe1,pipe2,timeFrame);
 
-
         procesabbles.add(packageByTimeFrameFilter);
         pipe1.accept(packageA);
+        pipe1.accept(packageB);
+        packageByTimeFrameFilter.process();
         packageByTimeFrameFilter.process();
         Assert.assertEquals(packageA,pipe2.retireve());
-
+        Assert.assertEquals(null,pipe2.retireve());
 
     }
 
