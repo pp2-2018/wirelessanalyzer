eb7eec8fcff66e8336ad99e922650a2e6d827294
LuluNull <lucylemonyay@gmail.com>
2018-04-27 14:47:15 -0300

    MacAddressesDetectedBySniffer filter added and tests

---
+++ src/main/java/pipeAndFilter/sink/MacAddressesDetectedBySniffer/MacAddressesDetectedBySniffer.java
@@ -0,0 +1,77 @@
+package pipeAndFilter.sink.MacAddressesDetectedBySniffer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Capture;
+import model.Package;
+import model.device.MacAddress;
+import model.device.roles.Sniffer;
+import negocio.iface.Obserbable;
+import pipeAndFilter.Pipe;
+import pipeAndFilter.impl.SinkImpl;
+
+public class MacAddressesDetectedBySniffer extends SinkImpl<Capture> implements Obserbable<negocio.iface.AnalyticsObserver>{
+	
+	private List<Capture> buffer;
+	private List<MacAddress> macs;
+	private Sniffer sniffer;
+	private List<negocio.iface.AnalyticsObserver> observers;
+	
+	public MacAddressesDetectedBySniffer(Pipe<Capture> inputPipe, Sniffer sniffer) {
+		super(inputPipe);
+		
+		buffer = new ArrayList<>();
+		this.sniffer = sniffer;
+		this.macs = new ArrayList<MacAddress>();
+		this.observers = new ArrayList<>();
+	}
+
+	@Override
+	public void take(Pipe<Capture> input) {
+		
+		if(input.canRetrieve()) {
+			buffer.add(input.retireve());
+			createMacsList();
+		}
+		else {
+			createMacsList();
+		}
+		
+	}
+	
+	private void createMacsList() {
+		for (Capture c : buffer){
+			
+			for (Package p : c.getPackages()){
+				if (c.getSniffer().equals(sniffer)){
+					macs.add(p.getMacAddress());
+					
+				}
+			}
+		}
+		
+		this.notifyAllObservers();
+	}
+	
+	public List<MacAddress> getMacs() {
+		return macs;
+	}
+
+	@Override
+	public void notifyAllObservers() {
+		
+		for (negocio.iface.AnalyticsObserver o : observers) {
+//			o.update(this);
+		}
+		
+	}
+
+	@Override
+	public void addObserver(negocio.iface.AnalyticsObserver o) {
+		
+		this.observers.add(o);
+		
+	}
+
+}
