5c96bbd752da64173be95ad6036e966472a02505
LuluNull <lucylemonyay@gmail.com>
2018-05-21 23:02:46 -0300

    TriangulatorBySniffer refactorized.

--- src/main/java/pipeAndFilter/filters/rawPackageFilter/TriangulatorBySniffer.java
+++ src/main/java/pipeAndFilter/filters/rawPackageFilter/TriangulatorBySniffer.java
@@ -11,7 +11,7 @@ import negocio.ConfigurationManager;
 import pipeAndFilter.Pipe;
 import pipeAndFilter.impl.SimpleFilterImpl;
 
-public class TriangulatorBySniffer extends SimpleFilterImpl<Package,Coordinates> { //que tipo devuelve?
+public class TriangulatorBySniffer extends SimpleFilterImpl<Package,Coordinates> {
 	
 	private Queue<Package> cola;
 	private ConfigurationMap configurationMap;
@@ -22,6 +22,7 @@ public class TriangulatorBySniffer extends SimpleFilterImpl<Package,Coordinates>
 		cola = new LinkedList<Package>();
 		configurationMap = ConfigurationManager.load();
 	}
+
 
 	
 @Override
@@ -34,64 +35,77 @@ public void transform(Pipe<Package> input, Pipe<Coordinates> output) {
     		cola.add(paquete);
     	}
     	
-    	if(cola.peek().getTimeStamp().equals(paquete.getTimeStamp())){		//debería tener un rango de aceptación de tiempo?
+    	if(cola.peek().getTimeStamp().equals(paquete.getTimeStamp())){
     		cola.add(paquete);
     	}
     	
-    	if(!cola.peek().getTimeStamp().equals(paquete.getTimeStamp())){		//falta el criterio que lo elimine cuando es muy viejo
+    	if(!cola.peek().getTimeStamp().equals(paquete.getTimeStamp())){
 
     		output.accept(triangular(cola));
     		cola.clear();
     		cola.add(paquete);
-    	}
-    
+    	} 
     }
 }
 
-public Coordinates triangular(Queue<Package> cola){
+
+
+public Coordinates triangular(Queue<Package> cola){	
 	
 	if(cola.size()==1){
 		Coordinates coord = configurationMap.getCoordinates(cola.peek().getSniffer());
 		return coord;
 	}
-
 	Package packageA = cola.poll();
 	Package packageB = cola.poll();
 
-	double sumaRangos = packageA.getSniffer().getRangeInMeters() + packageB.getSniffer().getRangeInMeters();
-	//
+	double coordenadaX = getCoordenadaX(packageA, packageB); 
+	double coordenadaY = getCoordenadaY(packageA, packageB);
 
+	return(new Coordinates(coordenadaX, coordenadaY));
+	
+	}
+
+
+
+public double getCoordenadaX(Package packageA, Package packageB){
+	
+	double sumaRangos = packageA.getSniffer().getRangeInMeters() + packageB.getSniffer().getRangeInMeters();
 	double distanciaX = Math.sqrt(Math.pow((configurationMap.getCoordinates(packageB.getSniffer()).getLat()
 			- configurationMap.getCoordinates(packageA.getSniffer()).getLat()), 2));	
 	
 	double interseccionX = (sumaRangos - distanciaX)/2;
-	double coordenadaX;
 	
+	double coordenadaX;
+
 	if(configurationMap.getCoordinates(packageB.getSniffer()).getLat() <= configurationMap.getCoordinates(packageA.getSniffer()).getLat()){
-		
 		coordenadaX = configurationMap.getCoordinates(packageB.getSniffer()).getLat() + interseccionX;	
 	}
 	else {
 		coordenadaX = configurationMap.getCoordinates(packageA.getSniffer()).getLat() + interseccionX;	
 	}
-	
-	//
+	return coordenadaX;
+}
 
+
+
+public double getCoordenadaY(Package packageA, Package packageB){
+	
+	double sumaRangos = packageA.getSniffer().getRangeInMeters() + packageB.getSniffer().getRangeInMeters();
 	double distanciaY = Math.sqrt(Math.pow((configurationMap.getCoordinates(packageB.getSniffer()).getLng()
 			- configurationMap.getCoordinates(packageA.getSniffer()).getLng()), 2));	
-			
-	double interseccionY = (sumaRangos - distanciaY)/2;
-	double coordenadaY;
 	
+	double interseccionY = (sumaRangos - distanciaY)/2;
+
+	double coordenadaY;
+
 	if(configurationMap.getCoordinates(packageB.getSniffer()).getLng() <= configurationMap.getCoordinates(packageA.getSniffer()).getLng()){
-		
 		coordenadaY = configurationMap.getCoordinates(packageB.getSniffer()).getLng() + interseccionY;	
 	}
 	else {
 		coordenadaY = configurationMap.getCoordinates(packageA.getSniffer()).getLng() + interseccionY;	
 	}
+	return coordenadaY;
+}
 
-	return(new Coordinates(coordenadaX, coordenadaY));
-	
-	}
 }
